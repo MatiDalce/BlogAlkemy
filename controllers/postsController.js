4dc807ce3c09d3a54ea05892486b9a66bf283dc7
Matias Dalceggio <matiasdalceggio@gmail.com>
2021-11-19 03:36:18 -0300

    all posts controller

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,14 +1,45 @@
 const db = require("../database/models")
 
 
-module.exports = { 
+module.exports = {
 
-getAll : (req,res) => { res.send("hola") }, 
-getPost : (req,res) => { },
+    getAll: async (req, res) => {
 
-postPost : (req,res) => { },
-editPost: (req,res) => {}, 
-delete: (req,res) => { }
+        const postss = await db.Post.findAll();
+
+        const post = []
+
+        postss.forEach(element => {
+
+            post.push({
+                id: element.id,
+                name: element.name,
+                title: element.title,
+                image: element.image,
+                category: element.category,
+                content: element.content,
+                detail: "http://localhost:3000/api/posts/"+element.id
+            })
+
+        });
+        res.json(post)
+    },
+
+    getPost: async (req, res) => {
+
+        const post = await db.Post.findByPk(req.params.id)
+
+        res.json(post)
+
+    },
+
+    postPost: (req, res) => { },
+    editPost: (req, res) => { },
+    delete: (req, res) => {
+
+
+
+    }
 
 
 }
