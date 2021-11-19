ef4bb70fa2a17bc3c0ba3a74216dc9a42700d291
Matias Dalceggio <matiasdalceggio@gmail.com>
2021-11-19 01:12:38 -0300

    routes/sequeelizeinstall/controllersfiles

---
+++ controllers/postsController.js
@@ -0,0 +1,11 @@
+module.exports = { 
+
+getAll : (req,res) => { res.send("hola") }, 
+getPost : (req,res) => { },
+
+postPost : (req,res) => { },
+editPost: (req,res) => {}, 
+delete: (req,res) => { }
+
+
+}
---
+++ routes/apiRouter.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router =  express.Router();
+const postsController = require("../controllers/postsController")
+
+
+router.get("/posts", postsController.getAll)
+router.get("/posts/:id", postsController.getPost)
+
+
+router.post("/posts", postsController.postPost) 
+
+
+router.patch("/posts/:id", postsController.editPost)
+
+router.delete("/posts/:id", postsController.delete)
+
+
+
+
+module.exports = router
