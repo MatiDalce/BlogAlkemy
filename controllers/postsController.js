56990370aaf92a443338b43293dae6dad83c7936
Matias Dalceggio <matiasdalceggio@gmail.com>
2021-11-20 04:58:59 -0300

    update method

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -18,24 +18,26 @@ module.exports = {
                 image: element.image,
                 category: element.category,
                 content: element.content,
-                detail: "http://localhost:3000/api/posts/"+element.id
+                detail: "http://localhost:3000/api/posts/" + element.id
             })
 
         });
 
 
-        
-        const data = {count: post.length,
-                      posts: post}
+
+        const data = {
+            count: post.length,
+            posts: post
+        }
         res.json(data)
     },
 
     getOnePost: async (req, res) => {
-        
+
 
         const onePost = await db.Post.findByPk(req.params.id)
 
-        if(!onePost) { res.send("No hay posteos")}
+        if (!onePost) { res.send("No hay posteos") }
 
         console.log(onePost)
 
@@ -43,36 +45,72 @@ module.exports = {
 
     },
 
-    postPost:  async(req, res) => {
+    postPost: async (req, res) => {
 
-        await db.Post.Create(  
-          {  
-            name: req.body.name,
-            title: req.body.title,
-            image: req.body.image,
-            category: req.body.category,
-            content: req.body.content,
+        await db.Post.Create(
+            {
+                name: req.body.name,
+                title: req.body.title,
+                image: "/images/" + req.file.originalname,
+                category: req.body.category,
+                content: req.body.content,
 
-          }
+            }
 
 
         )
 
-        
-
-          
 
 
 
-     },
-    editPost: (req, res) => { },
-    delete:async (req, res) => {
+
+
+
+    },
+    editPost: async (req, res) => {
+
+        if (!req.body) { res.send("No se recibieron datos") }
+        if (!req.file) {
+            await db.Post.update({
+                name: req.body.name,
+                title: req.body.title,
+                category: req.body.category,
+                content: req.body.content,
+
+            }, {
+                where: {
+                    id: req.params.id
+                }
+            }
+            )
+        } else {
+
+            await db.Post.update({
+                name: req.body.name,
+                title: req.body.title,
+                image: "/images/" + req.file.originalname,
+                category: req.body.category,
+                content: req.body.content,
+
+            }, {
+                where: {
+                    id: req.params.id
+                }
+            }
+            )
+        }
+
+
+
+
+    },
+    delete: async (req, res) => {
 
         await db.Post.destroy({
-            where:{
-                id:req.params.id
+            where: {
+                id: req.params.id
             }
-           })
+        })
 
 
 
